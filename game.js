25b74151394ea39f73d8b4afb6c70e95c6248b34
alsanders86 <alsanders86@gmail.com>
2022-10-12 10:43:31 -0400

    Add player input functionality

--- game.js
+++ game.js
@@ -1,35 +1,48 @@
 const container = document.querySelector('.container');
 let button = document.querySelector('#grid-button');
+let gridNumber = 16;
 
+// button will clear the current grid and make a new grid
 button.addEventListener("click", function() {
-    // alert("he said the line!");
-    let gridNumber = prompt("enter how many rows/columns you want");
+    clearGrid();
+
+    do {
+        gridNumber = parseInt(window.prompt("enter how many rows/columns you want. (a number between 1-100)", ""));
+    } while(isNaN(gridNumber) || gridNumber > 100 || gridNumber < 1);
+
+    drawGrid(gridNumber);
 });
-// for 16 rows, make the row. within that row, add 16 grid pieces/columns
-for(i = 0; i < 16; i++) {
-    const gridRow = document.createElement('div');
-    gridRow.setAttribute('class', 'row');
-    // gridRow.textContent = "general kenobi";
 
-    for(j = 0; j < 16; j++) {
-        const gridPiece = document.createElement('div');
-        gridPiece.setAttribute('class', 'grid');
-        gridPiece.textContent = "hello there";
 
-        gridPiece.addEventListener("mouseover", function() {
-            gridPiece.setAttribute("style", "background-color: white;");
-            gridPiece.textContent = "general kenobi";
-        }, false);
 
-        gridRow.appendChild(gridPiece);
+function drawGrid(gridNumber) {
+    // for 16 rows, make the row. within that row, add 16 grid pieces/columns
+    for(i = 0; i < gridNumber; i++) {
+        const gridRow = document.createElement('div');
+        gridRow.setAttribute('class', 'row');
+        // gridRow.textContent = "general kenobi";
+
+        for(j = 0; j < gridNumber; j++) {
+            const gridPiece = document.createElement('div');
+            gridPiece.setAttribute('class', 'grid');
+            // gridPiece.textContent = "hello there";
+
+            gridPiece.addEventListener("mouseover", function() {
+                gridPiece.setAttribute("style", "background-color: white;");
+                // gridPiece.textContent = "general kenobi";
+            }, false);
+
+            gridRow.appendChild(gridPiece);
+        }
+
+        container.appendChild(gridRow);
     }
-
-    container.appendChild(gridRow);
 }
 
-
-function drawGrid() {
-
+function clearGrid() {
+    while(container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+    }
 }
 
-drawGrid();
+drawGrid(gridNumber);
